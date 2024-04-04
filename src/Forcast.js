11dc3165d8eaaa59b1e6ebc3261cc2da115da576
loliver42 <156827618+loliver42@users.noreply.github.com>
2024-04-04 10:43:46 -0400

    restarting

--- src/Forcast.js
+++ src/Forcast.js
@@ -1,37 +1,28 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Forcast.css";
 import axios from "axios";
 
 export default function Forcast(props) {
-  let [loaded, setLoaded] = useState(false);
-  let [forcastData, setForcastData] = useState(null);
   function displayForcast(response) {
-    setForcastData(response.data.daily);
-
-    setLoaded(true);
+    console.log(response.data);
   }
+  let apiKey = "ffa4fe680act3be1832a3445o0790076";
+  let lon = props.coords.lon;
+  let lat = props.coords.lat;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${lon}&lat=${lat}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayForcast);
 
-  if (loaded) {
-    return (
-      <div className="Forcast">
-        <div className="row">
-          <div className="col">Thurs</div>
-          <div></div>
-          <div className="Forcast-temperatures">
-            {" "}
-            <span className="Forcast-temp-min">{forcastData}°</span> /{" "}
-            <span className="Forcast-temp-max">{forcastData} °</span>
-          </div>
+  return (
+    <div className="Forcast">
+      <div className="row">
+        <div className="col">Thurs</div>
+        <div></div>
+        <div className="Forcast-temperatures">
+          {" "}
+          <span className="Forcast-temp-min">°</span> /{" "}
+          <span className="Forcast-temp-max"> °</span>
         </div>
       </div>
-    );
-  } else {
-    let apiKey = "ffa4fe680act3be1832a3445o0790076";
-    let lon = props.coords.lon;
-    let lat = props.coords.lat;
-    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${lon}&lat=${lat}&key=${apiKey}&units=imperial`;
-    axios.get(apiUrl).then(displayForcast);
-
-    return " Loading Forcast...";
-  }
+    </div>
+  );
 }
